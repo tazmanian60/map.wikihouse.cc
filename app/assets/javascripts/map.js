8c26c91fe5752c47b147234e407ad4675c1121c7
John Rees <john@bitsushi.com>
2016-10-17 14:41:21 +0100

    no scroll on embedded map and link opens in parent window

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,6 +1,6 @@
 document.addEventListener("turbolinks:load", function() {
   if ( $('#map').length > 0) {
-    var map = L.map('map').setView([51.505, -0.09], 2)
+    var map = L.map('map', { scrollWheelZoom: !$('#map').hasClass('embedded') }).setView([51.505, -0.09], 2)
     L.tileLayer('https://api.mapbox.com/styles/v1/wikihousefoundation/ciu6xn84l001w2hljioetinhk/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2lraWhvdXNlZm91bmRhdGlvbiIsImEiOiJjaXU2eG1sNG0wMDFuMnRwaXZjYXoxdzJzIn0.WOHKQ7ExpKD81-DG2-smWg', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18
