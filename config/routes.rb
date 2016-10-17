0eedf04669fa635d738442789ffa31dfac0578fd
John Rees <john@bitsushi.com>
2016-10-17 15:04:45 +0100

    terms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'signout', to: 'sessions#destroy'
-
+  get 'terms', to: 'static#terms'
   resources :places
   resource :embed
 
