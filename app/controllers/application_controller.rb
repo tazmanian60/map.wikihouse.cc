8023b9e103df2e1acfadb332283ef47f7bc9afdc
John Rees <john@bitsushi.com>
2016-10-18 15:41:12 +0100

    trying to fix csrf problem http://stackoverflow.com/a/27416404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   private
 
