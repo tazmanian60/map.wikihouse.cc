44dcb843fe9b5f9dc5e11a331a278b0a33cd04e8
John Rees <john@bitsushi.com>
2016-10-18 14:48:50 +0100

    min/max zoon

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,7 +3,12 @@
 
 document.addEventListener("turbolinks:load", function() {
   if ( $('#map').length > 0) {
-    var map = L.map('map', { scrollWheelZoom: !$('#map').hasClass('embedded') }).setView([51.505, -0.09], 2)
+    var mapOptions = {
+      scrollWheelZoom: !$('#map').hasClass('embedded'),
+      minZoom: 3,
+      maxZoom: 10
+    }
+    var map = L.map('map', mapOptions).setView([51.505, -0.09], 3)
     L.instagram('https://api.instagram.com/v1/tags/wikihouse/media/recent?access_token=4049407803.79d3ccb.d1b78cc92ebc414389bf720278f87da1').addTo(map)
 
     L.tileLayer('https://api.mapbox.com/styles/v1/wikihousefoundation/ciu6xn84l001w2hljioetinhk/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2lraWhvdXNlZm91bmRhdGlvbiIsImEiOiJjaXU2eG1sNG0wMDFuMnRwaXZjYXoxdzJzIn0.WOHKQ7ExpKD81-DG2-smWg', {
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protect_from_forgery with: :exception
 
   private
@@ -11,5 +13,10 @@ class ApplicationController < ActionController::Base
 
     def authenticate
       redirect_to '/auth/sign_in_with_slack' unless session[:user]
+    end
+
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to(request.referrer || root_path)
     end
 end
