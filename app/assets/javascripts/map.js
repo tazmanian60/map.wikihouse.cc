bf34b44d575356998ba8b9702dd0943aadd798f7
John Rees <john@bitsushi.com>
2016-10-26 14:14:17 +0100

    layers half work

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -9,17 +9,64 @@ document.addEventListener("turbolinks:load", function() {
       maxZoom: 10
     }
     var map = L.map('map', mapOptions).setView([51.505, -0.09], 3)
-
-    var geojsonLayer = new L.GeoJSON.AJAX("/places.json")
-    geojsonLayer.addTo(map)
-
-    L.instagram('https://api.instagram.com/v1/tags/wikihouse/media/recent?access_token=4049407803.79d3ccb.d1b78cc92ebc414389bf720278f87da1').addTo(map)
-
-    L.tileLayer('https://api.mapbox.com/styles/v1/wikihousefoundation/ciu6xn84l001w2hljioetinhk/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2lraWhvdXNlZm91bmRhdGlvbiIsImEiOiJjaXU2eG1sNG0wMDFuMnRwaXZjYXoxdzJzIn0.WOHKQ7ExpKD81-DG2-smWg', {
+    var mapbox = L.tileLayer('https://api.mapbox.com/styles/v1/wikihousefoundation/ciu6xn84l001w2hljioetinhk/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2lraWhvdXNlZm91bmRhdGlvbiIsImEiOiJjaXU2eG1sNG0wMDFuMnRwaXZjYXoxdzJzIn0.WOHKQ7ExpKD81-DG2-smWg', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18
-    }).addTo(map)
+    })
+    mapbox.addTo(map)
 
+    var mapLayerGroups = [];
     // map.once('focus', function() { map.scrollWheelZoom.enable(); });
+
+
+    // places layer
+
+    var geoJSONOptions = {
+      onEachFeature: onEachFeature,
+      filter: onEachFilter
+    }
+
+    var geojsonLayer = new L.GeoJSON.AJAX("/places.json", geoJSONOptions)
+
+    // var places = geojsonLayer.refilter(function(feature){
+    //     return feature.properties.amenity === "Place";
+    // });
+
+    function onEachFeature(feature, layer) {
+      var lg = mapLayerGroups[feature.properties.amenity];
+      if (lg === undefined) {
+        lg = new L.layerGroup();
+        //add the layer to the map
+        lg.addTo(map);
+        //store layer
+        mapLayerGroups[feature.properties.amenity] = lg;
+      }
+      lg.addLayer(layer);
+
+      if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+      }
+    }
+
+    function onEachFilter(feature, layer) {
+      return true;
+      // return feature.properties.amenity == "Place";
+    }
+
+
+    // instagram layer
+    var instagram = L.instagram('https://api.instagram.com/v1/tags/wikihouse/media/recent?access_token=4049407803.79d3ccb.d1b78cc92ebc414389bf720278f87da1')
+    instagram.addTo(map)
+
+
+    var baseLayers = {
+      // "Mapbox": mapbox
+    };
+    var overlays = {
+      "Instagram Photos": instagram,
+      "Places": geojsonLayer
+    };
+    L.control.layers(baseLayers, overlays).addTo(map);
   }
+
 })
