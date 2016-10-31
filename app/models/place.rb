e27028b0aa8bc33bf67809a5dfa830b6f41199b3
John Rees <john@bitsushi.com>
2016-10-31 14:39:08 +0000

    changed default

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,13 +3,13 @@ class Place < ApplicationRecord
 
   include Workflow
   workflow do
-    state :new do
-      event :submit, :transitions_to => :awaiting_review
-    end
+    # state :new do
+    #   event :submit, :transitions_to => :awaiting_review
+    # end
+    # state :awaiting_review do
+    #   event :review, :transitions_to => :being_reviewed
+    # end
     state :awaiting_review do
-      event :review, :transitions_to => :being_reviewed
-    end
-    state :being_reviewed do
       event :accept, :transitions_to => :accepted
       event :reject, :transitions_to => :rejected
     end
