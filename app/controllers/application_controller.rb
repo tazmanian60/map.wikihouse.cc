17a88e41a80219a229a884e6492c0c5a13cf44c5
Jon Leighton <j@jonathanleighton.com>
2017-03-15 20:23:54 +0000

    Remove unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-    def current_user
-      @current_user ||= session[:user] if session[:user]
-    end
-    helper_method :current_user
-
-    def authenticate
-      redirect_to '/auth/sign_in_with_slack' unless session[:user]
-    end
-
     def user_not_authorized
       flash[:alert] = "You are not authorized to perform this action."
       redirect_to(request.referrer || root_path)
