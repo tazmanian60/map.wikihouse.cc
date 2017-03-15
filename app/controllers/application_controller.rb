6d321a85277ea8fc1a4b5f56395e1cc69c608d23
Jon Leighton <j@jonathanleighton.com>
2017-03-15 20:22:37 +0000

    Enable CSRF protection
    Closes #8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  # protect_from_forgery with: :exception
-  # protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
 
   private
 
