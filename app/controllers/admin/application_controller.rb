13fca12cb62dfd81e5c9334b444c7d3deb5c37d6
Jon Leighton <j@jonathanleighton.com>
2017-03-16 16:20:59 +0000

    Add basic auth to admin
    Closes #5

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,7 @@
 class Admin::ApplicationController < ApplicationController
   layout "admin"
+
+  if password = ENV["admin_password"]
+    http_basic_authenticate_with name: "wikihouse", password: password
+  end
 end
