26e184de2ba71baa5de6d0a5d949117ad1b0e7b5
Jon Leighton <j@jonathanleighton.com>
2017-04-07 11:41:23 +0100

    Map .place-address map initialization idempotent
    Otherwise we potentially get multiple copies of the map on page
    navigation (i.e. when navigating back with turbolinks)

--- app/assets/javascripts/admin/place_address.js
+++ app/assets/javascripts/admin/place_address.js
@@ -1,6 +1,11 @@
 document.addEventListener("turbolinks:load", function() {
   $(".place-address").each(function() {
     var address = $(this);
+
+    if (address.find(".place-map").length) {
+      return
+    }
+
     var coords = [address.data("lat"), address.data("lng")];
 
     var map = L.map($("<div>", { class: "place-map" }).appendTo(address)[0]);
